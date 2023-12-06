980e3f1bb279fdc7a8973f93baf6349fea949ebe
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2023-12-06 18:33:50 +0200

    Finished customers Added services and controllers to employee, orders and products

--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+import { EmployeesController } from './employees.controller';
 
-@Module({})
+@Module({
+  providers: [EmployeesService],
+  controllers: [EmployeesController]
+})
 export class EmployeesModule {}
